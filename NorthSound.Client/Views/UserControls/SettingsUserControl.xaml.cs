aa853928032ac52bad2abcd8202aa0895c648dda
Я шингиз <PHPpriest@gmail.com>
2022-11-04 01:04:43 +0500

    Очистка кода

--- NorthSound.Client/Views/UserControls/SettingsUserControl.xaml.cs
+++ NorthSound.Client/Views/UserControls/SettingsUserControl.xaml.cs
@@ -1,28 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace NorthSound.Client.Views.UserControls
+namespace NorthSound.Client.Views.UserControls;
+public partial class SettingsUserControl : UserControl
 {
-    /// <summary>
-    /// Логика взаимодействия для SettingsUserControl.xaml
-    /// </summary>
-    public partial class SettingsUserControl : UserControl
-    {
-        public SettingsUserControl()
-        {
-            InitializeComponent();
-        }
-    }
+    public SettingsUserControl() => InitializeComponent();
+        
 }
