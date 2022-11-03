b7da0755e83d5ee5c51b4db83e34ab2a18deac1c
Я шингиз <PHPpriest@gmail.com>
2022-11-04 04:02:24 +0500

    Базовые команды для вью

---
+++ NorthSound.Client/Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NorthSound.Client.Infrastructure.Commands.Base;
+
+public abstract class CommandBase : ICommand
+{
+    /// <summary>
+    /// Вызывается при изменении возвращаемого значения
+    /// метода CanExecute
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <summary>
+    /// Метод проверяющий возможность исполнения команды.
+    /// Если метод возвращает <see langword="false"/> - элемент 
+    /// управления, вызывавший команду, станет недоступен.
+    /// </summary>
+    /// <param name="parameter"></param>
+    public abstract bool CanExecute(object? parameter);
+
+    /// <summary>
+    /// Метод, который будет вызван при срабатывании команды.
+    /// </summary>
+    /// <param name="parameter">Параметры команды</param>
+    public abstract void Execute(object? parameter);
+}
---
+++ NorthSound.Client/Infrastructure/Commands/SwitchWindowState.cs
@@ -0,0 +1,26 @@
+﻿using NorthSound.Client.Infrastructure.Commands.Base;
+using System.Diagnostics;
+using System.Windows;
+
+namespace NorthSound.Client.Infrastructure.Commands;
+
+internal class SwitchWindowState : CommandBase
+{
+    private bool _isMaximized = false;
+
+    public override bool CanExecute(object? parameter) => true;
+
+    public override void Execute(object? parameter)
+    {
+        if (_isMaximized)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+        else
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+        }
+
+        _isMaximized = !_isMaximized;
+    }
+}
