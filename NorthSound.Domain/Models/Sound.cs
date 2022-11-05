46f18a38a3e306a9c5e2de4f1a6ee9fbe8231a8b
Я шингиз <PHPpriest@gmail.com>
2022-11-06 02:37:13 +0500

    Фикс багов динам. растяжения

--- NorthSound.Domain/Models/Sound.cs
+++ NorthSound.Domain/Models/Sound.cs
@@ -1,19 +1,19 @@
-﻿namespace NorthSound.Domain.Models;
+﻿using System.Xml.Linq;
+
+namespace NorthSound.Domain.Models;
 
 public class Sound
 {
-	public Sound() { }
+	public Sound()
+	{
+        Name = string.IsNullOrWhiteSpace(Name) ? "Неизвестно" : Name;
+        Author = string.IsNullOrWhiteSpace(Author) ? "Неизвестно" : Author;
+    }
 
 	public string GeneratedTitle
 	{
-		get
-		{
-			var name = string.IsNullOrWhiteSpace(Name) ? "Неизвестно" : Name;
-			var author = string.IsNullOrWhiteSpace(Author) ? "Неизвестно" : Author;
-
-			return $"{name} - {author}";
-		}
-	}
+		get => $"{Name} - {Author}";
+    }
 
 	public string Name 
 	{ 
@@ -45,25 +45,25 @@ public class Sound
 		set;
 	}
 
-    public bool IsAnyPropsContains(string filter)
-    {
-        return
-            Name.ToLower().Contains(filter.ToLower()) ||
-            Author.ToLower().Contains(filter.ToLower());
-    }
+	public bool IsAnyPropsContains(string filter)
+	{
+		return
+			Name.ToLower().Contains(filter.ToLower()) ||
+			Author.ToLower().Contains(filter.ToLower());
+	}
 
-    public static Sound[] GetTemplateAudios()
-    {
-        return new Sound[]
-        {
-            new Sound() { Name = "Seven Nation Army", Author = "" },
-            new Sound() { Name = "Sweet Dreams", Author = "Marilyn Manson" },
-            new Sound() { Name = "Закройте", Author = "лампабикт" },
-            new Sound() { Name = "Пачка сигарет", Author = "Виктор Цой - КИНО" },
-            new Sound() { Name = "Пачка сигарет", Author = "Lizer" },
-            new Sound() { Name = "ОЙДА", Author = "Oxxxymiron" },
-        };
-    }
+	public static Sound[] GetTemplateAudios()
+	{
+		return new Sound[]
+		{
+			new Sound() { Name = "Seven Nation Army" },
+			new Sound() { Name = "Sweet Dreams", Author = "Marilyn Manson" },
+			new Sound() { Name = "Закройте", Author = "лампабикт" },
+			new Sound() { Name = "Пачка сигарет", Author = "Виктор Цой - КИНО" },
+			new Sound() { Name = "Пачка сигарет", Author = "Lizer" },
+			new Sound() { Name = "ОЙДА", Author = "Oxxxymiron" },
+		};
+	}
 
 	public override string ToString()
 	{
