fe00682cbedd1535621f1508854c47333aa27cc8
Я шингиз <PHPpriest@gmail.com>
2022-11-06 04:08:28 +0500

    Дополнение вьюмодели окна

--- NorthSound.Client/ViewModels/MainWindowViewModel.cs
+++ NorthSound.Client/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using NorthSound.Client.ViewModels.Base;
+using System.Windows.Controls;
 
 namespace NorthSound.Client.ViewModels;
 
 internal class MainWindowViewModel : ViewModelBase
 {
     private string _title = "";
+    private TabItem _selectedTabItem;
 
     public MainWindowViewModel()
     {
@@ -15,6 +17,16 @@ internal class MainWindowViewModel : ViewModelBase
     {
         get => _title;
         set => Set(ref _title, value);
+    }
+
+    public TabItem SelectedTabItem
+    {
+        get => _selectedTabItem;
+        set
+        {
+            Set(ref _selectedTabItem, value);
+            Title = $"North Audio - {value.Name}";
+        }
     }
 }
 
