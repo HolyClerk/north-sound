4bb8ad389844c860ef31720c82d96fac208ca1f4
Я шингиз <PHPpriest@gmail.com>
2022-11-06 01:10:09 +0500

    биндинг данных аудио

--- NorthSound.Client/ViewModels/SoundViewModel.cs
+++ NorthSound.Client/ViewModels/SoundViewModel.cs
@@ -1,47 +1,39 @@
 ﻿using NorthSound.Client.ViewModels.Base;
 using NorthSound.Domain.Models;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Data;
 
 namespace NorthSound.Client.ViewModels;
 
 internal class SoundViewModel : ViewModelBase
 {
-    private string? _selectedAudioTitle;
-    private string? _filterText;
+    private string _filterText = "";
     private ICollectionView? _audioPlaylistItems;
-    private Sound _selectedAudio;
+    private Sound? _selectedAudio;
 
     public SoundViewModel()
     {
-        SelectedAudioTitle = "Test - test";
-
         AudioPlaylistItems = CollectionViewSource.GetDefaultView(Sound.GetTemplateAudios());
         AudioPlaylistItems.Filter = FilterAudio;
     }
 
-    public string? FilterText
+    public string FilterText
     {
         get => _filterText;
         set => Set(ref _filterText, value);
     }
 
-    public string? SelectedAudioTitle
+    public ICollectionView? AudioPlaylistItems
     {
-        get => _selectedAudioTitle;
-        set => Set(ref _selectedAudioTitle, value);
-    }
-
-    public ICollectionView AudioPlaylistItems
-    {
-        get => _audioPlaylistItems; 
+        get => _audioPlaylistItems;
         set => Set(ref _audioPlaylistItems, value);
     }
 
-    public Sound SelectedAudio
+    public Sound? SelectedAudio
     {
         get => _selectedAudio;
-        set => Set(ref _selectedAudio, value);
+        set => Set(ref _selectedAudio, value);  
     }
 
     private bool FilterAudio(object obj)
