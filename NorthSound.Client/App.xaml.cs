5f1de659e487ceced78d2ffee26aebe27d52c2f5
Dmitry Kovalev <phppriest@gmail.com>
2023-01-25 13:00:24 +0500

    Добавление DI-контейнера

--- NorthSound.Client/App.xaml.cs
+++ NorthSound.Client/App.xaml.cs
@@ -1,17 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NorthSound.Client.ViewModels;
+using NorthSound.Client.ViewModels.Interfaces;
 using System.Windows;
 
 namespace NorthSound.Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<MainWindow>()
+                        .AddSingleton<ApplicationViewModel>()
+                        .AddSingleton<ISongViewModel, SongViewModel>();
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.DataContext = _host.Services.GetRequiredService<ApplicationViewModel>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
     }
 }
