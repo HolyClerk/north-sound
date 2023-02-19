fa1d9ffa945459313c888f4b335de2437711a3b0
Dmitry Kovalev <phppriest@gmail.com>
2023-02-20 02:45:55 +0500

    Добавление анотаций данных в сущность

--- NorthSound.Domain/Models/Song.cs
+++ NorthSound.Domain/Models/Song.cs
@@ -1,11 +1,22 @@
-﻿namespace NorthSound.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthSound.Domain.Models;
 
 public class Song
 {
+    [Key] 
     public int Id { get; set; } = default;
+
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; } = default!;
+
+    [Required]
+    [StringLength(100)]
     public string Author { get; set; } = default!;
-    public Uri Path { get; set; } = default!;
+
+    public Uri? Path { get; set; } = default!;
 
     public bool IsAnyPropsContains(string filter)
     {
