2df87c9ef84dc277eba574027b0410521bdfe2f7
Dmitry Kovalev <phppriest@gmail.com>
2023-04-13 20:50:21 +0500

    Добавление возможности использования параметров в Execute асинхронного RelayCommand

--- NorthSound.Infrastructure/Commands/Base/AsyncRelayCommand.cs
+++ NorthSound.Infrastructure/Commands/Base/AsyncRelayCommand.cs
@@ -5,10 +5,10 @@ namespace NorthSound.Infrastructure.Commands.Base;
 
 public class AsyncRelayCommand : CommandBase
 {
-    private readonly Func<Task> _execute;
+    private readonly Func<object?, Task> _execute;
     private readonly Func<object, bool>? _canExecute;
 
-    public AsyncRelayCommand(Func<Task> execute, Func<object, bool>? canExecute = null)
+    public AsyncRelayCommand(Func<object?, Task> execute, Func<object, bool>? canExecute = null)
     {
         _execute = execute ?? throw new ArgumentNullException(nameof(execute));
         _canExecute = canExecute;
@@ -18,5 +18,5 @@ public class AsyncRelayCommand : CommandBase
         => _canExecute?.Invoke(parameter!) ?? true;
 
     public override async void Execute(object? parameter) 
-        => await _execute();
+        => await _execute(parameter);
 }
--- NorthSound.Infrastructure/Commands/Base/RelayCommand.cs
+++ NorthSound.Infrastructure/Commands/Base/RelayCommand.cs
@@ -13,7 +13,9 @@ public class RelayCommand : CommandBase
         _canExecute = canExecute;
     }
 
-    public override bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter!) ?? true;
+    public override bool CanExecute(object? parameter) 
+        => _canExecute?.Invoke(parameter!) ?? true;
 
-    public override void Execute(object? parameter) => _execute(parameter!);
+    public override void Execute(object? parameter) 
+        => _execute(parameter!);
 }
