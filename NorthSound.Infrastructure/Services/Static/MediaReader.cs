b08b0bc59a5ed83a4155e3364150655f6cfca619
Dmitry Kovalev <phppriest@gmail.com>
2023-04-14 14:56:30 +0500

    Вспомогательный функционал

--- NorthSound.Infrastructure/Services/Static/MediaReader.cs
+++ NorthSound.Infrastructure/Services/Static/MediaReader.cs
@@ -9,6 +9,9 @@ namespace NorthSound.Infrastructure.Services.Static;
 
 public static class MediaReader
 {
+    public static string MusicPath => Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + @"\NorthSound\";
+    public static string DownloadPath => Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\";
+
     // Конвертирует метаданные файла в модель Song
     public static async Task<SongModel?> ConvertMetadataAsync(string songPath)
     {
@@ -28,7 +31,7 @@ public static class MediaReader
             return null;
         }
 
-        return new LocalSong()
+        return new SongFile()
         {
             Name = Encoding.Default.GetString(metadataBuffer, 3, 30),
             Author = Encoding.Default.GetString(metadataBuffer, 33, 30),
@@ -37,9 +40,9 @@ public static class MediaReader
     }
 
     // Конвертирует title файла в модель Song
-    public static LocalSong ConvertToSong(FileInfo songInfo)
+    public static SongFile ConvertToSong(FileInfo songInfo)
     {
-        var songTemplate = new LocalSong()
+        var songTemplate = new SongFile()
         {
             Author = "None",
             Name = "None",
---
+++ NorthSound.Infrastructure/Services/Static/VirtualSongConverter.cs
@@ -0,0 +1,14 @@
+﻿using NorthSound.Domain.Models;
+using System;
+
+namespace NorthSound.Infrastructure.Services.Static;
+
+public static class VirtualSongConverter
+{
+    // TODO: Убрать хардкод
+    public const string DownloadApiPath = "http://localhost:5000/api/stream/";
+    public const string StreamApiPath = "http://localhost:5000/api/stream/";
+
+    public static Uri GetUriLinkToStream(VirtualSong virtualSong)
+        => new(StreamApiPath + virtualSong.Id);
+}
