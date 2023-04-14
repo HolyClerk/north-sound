9d8252fb002cbfd7539568ab0a38868f2bc14905
Dmitry Kovalev <phppriest@gmail.com>
2023-04-14 15:15:11 +0500

    Разнос классов по соответствующим директориям

--- NorthSound.Infrastructure/Services/Storage/Base/IObservableStorage.cs
+++ NorthSound.Infrastructure/Services/Storage/Base/IObservableStorage.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace NorthSound.Infrastructure.Services.Base;
+namespace NorthSound.Infrastructure.Services.Storage.Base;
 
 public interface IObservableStorage<T>
 {
---
+++ NorthSound.Infrastructure/Services/Storage/SongStorageService.cs
@@ -0,0 +1,35 @@
+﻿using NorthSound.Domain.Models;
+using NorthSound.Infrastructure.Services.Storage.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NorthSound.Infrastructure.Services.Storage;
+
+public class SongStorageService : ICollectionObserver<SongModel>, IObservableStorage<SongModel>
+{
+    /// <summary>
+    /// Класс представляет собой текущую коллекцию, избранную на воспроизведение.
+    /// </summary>
+    public SongStorageService()
+    {
+        Collection = new();
+    }
+
+    public ObservableCollection<SongModel> Collection
+    {
+        get;
+        private set;
+    }
+
+    public void ChangeObservableCollection(IEnumerable<SongModel> newObservableCollection)
+    {
+        Collection.Clear();
+
+        foreach (var item in newObservableCollection)
+        {
+            Collection.Add(item);
+        }
+    }
+}
