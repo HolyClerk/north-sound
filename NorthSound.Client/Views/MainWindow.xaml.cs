9b7e7beef9b3d83490bd78f27a9cf5dba55ab3c3
Holy Clerk <phppriest@gmail.com>
2023-05-18 02:20:10 +0500

    Функционал кнопок закрытия, сворачивания и разворачивания

--- NorthSound.Client/Views/MainWindow.xaml.cs
+++ NorthSound.Client/Views/MainWindow.xaml.cs
@@ -13,4 +13,25 @@ public partial class MainWindow : Window
             this.DragMove();
         }
     }
+
+    private void Minimize(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void Close(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void Maximize(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            return;
+        }
+
+        WindowState = WindowState.Maximized;
+    }
 }
