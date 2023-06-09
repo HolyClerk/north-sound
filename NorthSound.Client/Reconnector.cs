00d3495a1132ce5b9ed20814cd3fcff60dd9421e
Holy Clerk <phppriest@gmail.com>
2023-06-09 20:20:57 +0500

    Reconnector

---
+++ NorthSound.Client/Reconnector.cs
@@ -0,0 +1,31 @@
+﻿using NorthSound.BLL.Tokens;
+using NorthSound.BLL.Web.Base;
+using NorthSound.Domain.POCO;
+using NorthSound.Infrastructure;
+using System.Threading.Tasks;
+
+namespace NorthSound.Client;
+
+public class Reconnector
+{
+    private readonly IHubService _hubService;
+
+    public Reconnector(ITokenStorage tokenStorage, IHubService service)
+    {
+        tokenStorage.TokenUpdated += OnTokenUpdated;
+        _hubService = service;
+    }
+
+    private async void OnTokenUpdated(JwtToken token)
+    {
+        await ReconnectChat(token);
+    }
+
+    private async Task ReconnectChat(JwtToken token)
+    {
+        var info = new ServerInfo();
+        var mediator = new ChatMediator(info.GetChatUrl(), token.Token);
+
+        await _hubService.SetChatConnectionAsync(mediator);
+    }
+}
