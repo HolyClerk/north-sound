2421dd357dc6bb6f284891da802efddfeddcdce3
Holy Clerk <phppriest@gmail.com>
2023-06-09 20:17:05 +0500

    Возможность отслеживания токенов

--- NorthSound.BLL/Tokens/TokenStorage.cs
+++ NorthSound.BLL/Tokens/TokenStorage.cs
@@ -1,4 +1,5 @@
 ﻿using NorthSound.Domain.POCO;
+using System;
 
 namespace NorthSound.BLL.Tokens;
 
@@ -6,8 +7,11 @@ public class TokenStorage : ITokenStorage
 {
     public JwtToken? ActualToken { get; private set; }
 
+    public event Action<JwtToken>? TokenUpdated;
+
     public void UpdateToken(JwtToken token)
     {
         ActualToken = token;
+        TokenUpdated?.Invoke(token);
     }
 }
