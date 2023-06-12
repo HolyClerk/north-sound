f7b0be05124f45daa703bab2b41a3863d9c1221f
Holy Clerk <phppriest@gmail.com>
2023-06-12 18:38:43 +0500

    Account info

---
+++ NorthSound.Domain/AccountInformation.cs
@@ -0,0 +1,14 @@
+﻿namespace NorthSound.Domain;
+
+public class AccountInformation
+{
+    public AccountInformation(string username)
+    {
+        Username = username;
+        StartUpDate = DateTime.Now;
+    }
+
+    public string Username { get; private set; }
+
+    public DateTime StartUpDate { get; }
+}
